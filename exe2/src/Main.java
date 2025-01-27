29a7b5a869249346c4b0fc32741c9c21ba8bb1cb
Sprite <139699268+KhronoLogix@users.noreply.github.com>
2025-01-27 13:38:48 -0500

    Add files via upload

---
+++ exe2/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int temp[] = {45, 29, 10, 22, 41, 28, 33};
+        int ror[] = {95, 60, 25, 5, 0, 75, 90};
+        String days[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+
+        for (int i = 1; i <= 6; i++) {
+            if (temp[i] > 32 && ror[i] < 50){
+                System.out.println("it's likely snow on " + days[i]);
+            } else if (ror[i] > 50) {
+                System.out.println("its likely to rain on " + days[i]);
+            }else {
+                System.out.println("theres no rain or snow chance on " + days[i]);
+            }
+        }
+    }
+}
